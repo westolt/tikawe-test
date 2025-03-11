e65f21ad2c27aaad9864f6a57c863011cb4262ab
westolt <williamstolt15@gmail.com>
2025-03-11 15:59:41 +0200

    Update application

--- app.py
+++ app.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    return "Tervetuloa sovellukseen!"
+
+@app.route("/page1")
+def page1():
+    return "Ensimmäinen sivu"
+
+@app.route("/page2")
+def page2():
+    return "Toinen sivu"
